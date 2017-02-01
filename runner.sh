43e9b71cd786e0c5af2ce4f2d4d99d9559052aff
Pavol Noha <pavol.noha@gmail.com>
2017-02-01 15:14:07 +0100

    added docker.sock volume for registered docker runner in order to use docker cli

--- runner.sh
+++ runner.sh
@@ -27,6 +27,7 @@ yes '' | gitlab-runner register --url ${gitlab_service_url} \
                                 --output-limit "20480" \
                                 --docker-image "docker:latest" \
                                 --docker-volumes /root/m2:/root/.m2 \
+                                --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
                                 --docker-extra-hosts ${GITLAB_HOST}:${GITLAB_IP}
 
 # assign runner token
